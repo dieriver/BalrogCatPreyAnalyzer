08de44f0bff0f3b26ef889f213655595addfde28
Diego Rivera <dieriverav@gmail.com>
2023-08-27 12:24:49 +0200

    Generalize paths in the starter script

--- catCam_starter.sh
+++ catCam_starter.sh
@@ -3,7 +3,7 @@
 echo "Executing CatPreyAnalyzer"
 # Tensorflow Stuff
 #export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.7/dist-packages:/home/pi/tensorflow1/models/research:/home/pi/tensorflow1/models/research/slim:/home/pi/.local/lib/python3.7/site-packages
-export PYTHONPATH=$PYTHONPATH:/home/pi/tensorflow1/models/research:/home/pi/tensorflow1/models/research/slim
+export PYTHONPATH=$PYTHONPATH:$HOME/tensorflow1/models/research:$HOME/tensorflow1/models/research/slim
 cd /home/pi/CatPreyAnalyzer
 while true; do
 	python3 cascade.py
