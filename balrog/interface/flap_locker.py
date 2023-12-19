dd865f969b44e1ed60c1f0afc4a17a5cf6135f9c
Diego Rivera <dieriverav@gmail.com>
2023-12-19 15:24:00 +0100

    Add pytype for static type checking

--- balrog/interface/flap_locker.py
+++ balrog/interface/flap_locker.py
@@ -8,7 +8,7 @@ from surepy.entities.devices import Flap
 from surepy.entities.pet import Pet
 from surepy.enums import LockState, Location
 
-from balrog.utils import logger
+from balrog.utils.utils import logger
 
 
 class FlapLocker:
@@ -31,7 +31,7 @@ class FlapLocker:
             pets_data[registered_pet.name] = registered_pet.pet_id
         return pets_data
 
-    async def list_pets_data(self, telegram_bot):
+    async def list_pets_data(self, telegram_bot) -> None:
         # list with all pets
         pets: list[dict[str, Any]] = await self.surepy.sac.get_pets()
         message = f"I found this:"
@@ -42,7 +42,7 @@ class FlapLocker:
                         f"since: {location_since}")
         telegram_bot.send_text(message)
 
-    async def list_devices(self, telegram_bot):
+    async def list_devices(self, telegram_bot) -> None:
         # all entities as id-indexed dict
         entities: dict[int, SurepyEntity] = await self.surepy.get_entities()
 
@@ -53,7 +53,7 @@ class FlapLocker:
             telegram_bot.send_text(f"{device.type = } | {device.unique_id = } | {device.id = }")
             telegram_bot.send_text(f"{entities[device.parent_id].full_name = } | {entities[device.parent_id] = }\n")
 
-    async def get_lock_state(self):
+    async def get_lock_state(self) -> LockState:
         try:
             devices: list[SurepyDevice] = await self.surepy.get_devices()
             for device in devices:
@@ -66,7 +66,7 @@ class FlapLocker:
             logger.debug('WARNING: We assume that the old state was "LOCKED_OUT"')
             return LockState.LOCKED_OUT
 
-    async def set_moria_lock_state(self, state: LockState, telegram_bot):
+    async def set_moria_lock_state(self, state: LockState, telegram_bot) -> None:
         # list with all devices
         devices: list[SurepyDevice] = await self.surepy.get_devices()
         for device in devices:
@@ -77,28 +77,28 @@ class FlapLocker:
                 if result_lock and result_device:
                     telegram_bot.send_text('Done')
 
-    async def unlock_moria(self, telegram_bot):
+    async def unlock_moria(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.UNLOCKED, telegram_bot)
 
-    async def lock_moria_in(self, telegram_bot):
+    async def lock_moria_in(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.LOCKED_IN, telegram_bot)
 
-    async def lock_moria_out(self, telegram_bot):
+    async def lock_moria_out(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.LOCKED_OUT, telegram_bot)
 
     async def lock_moria(self, telegram_bot):
         await self.set_moria_lock_state(LockState.LOCKED_ALL, telegram_bot)
 
-    async def activate_curfew(self, telegram_bot):
+    async def activate_curfew(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.CURFEW, telegram_bot)
 
-    async def lock_moria_curfew(self, telegram_bot):
+    async def lock_moria_curfew(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.CURFEW_LOCKED, telegram_bot)
 
-    async def unlock_moria_curfew(self, telegram_bot):
+    async def unlock_moria_curfew(self, telegram_bot) -> None:
         await self.set_moria_lock_state(LockState.CURFEW_UNLOCKED, telegram_bot)
 
-    async def unlock_for_seconds(self, telegram_bot, seconds: int):
+    async def unlock_for_seconds(self, telegram_bot, seconds: int) -> None:
         old_state = await self.get_lock_state()
         logger.debug(f"Old state = {old_state}")
         if old_state >= LockState.CURFEW:
@@ -111,7 +111,7 @@ class FlapLocker:
         logger.debug(f"Setting back old state = {old_state}")
         await self.set_moria_lock_state(old_state, telegram_bot)
 
-    async def switch_pet_location(self, telegram_bot, pet_id: int):
+    async def switch_pet_location(self, telegram_bot, pet_id: int) -> None:
         pets: list[dict[str, Any]] = await self.surepy.sac.get_pets()
         if pets is None:
             telegram_bot.send_text(f"No pet was found int he server")
