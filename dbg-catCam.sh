50f4176a4b10b1ca6cb4b7af831461fee88e3fab
Diego Rivera <dieriverav@gmail.com>
2023-12-23 10:50:13 +0100

    Add start script for debugging purposes

---
+++ dbg-catCam.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "Executing CatPreyAnalyzer"
+# Tensorflow Stuff
+SNAP_ID = 1
+while : ; do
+  python3 -v -m balrog 2>&1 | tee /data/balrog-logs/stdout.log
+  case $? in
+  255)
+    echo "Exiting balrog script"
+    mkdir -p /data/balrog-snaps/snap$SNAP_ID
+    mv /data/balrog-logs/* /data/balrog-snaps/snap$SNAP_ID
+    ((SNAP_ID++))
+    break
+    ;;
+  *)
+    # Whatever else will be interpreted as a restart of teh script
+    echo "Restarting balrog script in 3 seconds"
+    mkdir -p /data/balrog-snaps/snap$SNAP_ID
+    mv /data/balrog-logs/* /data/balrog-snaps/snap$SNAP_ID
+    ((SNAP_ID++))
+    sleep 3
+    true
+    ;;
+  esac
+done
