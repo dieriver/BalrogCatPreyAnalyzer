cb6eb50a68664ed4cb84318ba7be32a9e7875451
Diego Rivera <dieriverav@gmail.com>
2024-07-30 17:24:00 +0200

    Remove code using old TF1 CC model

--- balrog/processor/model_stages.py
+++ balrog/processor/model_stages.py
@@ -1,104 +1,36 @@
-import os
-import pathlib
 import time
-from pathlib import Path
 from typing import Tuple, Sequence
 
 import cv2
+import kagglehub as hub
 import numpy as np
 import tensorflow as tf
-import kagglehub as hub
 from cv2.typing import MatLike
-from object_detection.utils import label_map_util
 
 from balrog.config import general_config
 from balrog.processor.cv_helpers import resize_img_to_square
 from balrog.types import Box
 from balrog.utils import logger, get_resource_path
 
-_tensorflow_models_path = os.getenv('BALROG_TENSORFLOW_PATH')
-if (_tensorflow_models_path is None or
-        len(_tensorflow_models_path) <= 0 or
-        not pathlib.Path(_tensorflow_models_path).is_dir()):
-    raise Exception("The BALROG_TENSOFLOW_PATH was not set, or points to an "
-                    "invalid location. Please check the assigned value")
-
-
 _PC_model_file = 'models/Prey_Classifier/0.86_512_05_VGG16_ownData_FTfrom15_350_Epochs_2020_05_15_11_40_56.h5'
 _FF_model_file = 'models/Face_Fur_Classifier/256_05_mobileNet_50_Epochs_2020_05_07_14_56_25.h5'
 _EYE_model_file = 'models/Eye_Detector/trainwhole100_Epochs_2020_04_30_18_05_25.h5'
 _HAAR_model_file = 'models/Haar_Classifier/haarcascade_frontalcatface_extended.xml'
 
-_TF_OD_model_name = 'ssdlite_mobilenet_v2_coco_2018_05_09'
-_TF_OD_frozen_model_filename = 'frozen_inference_graph.pb'
-_TF_OD_labels_filename = 'data/mscoco_label_map.pbtxt'
-
-_CR_model_file = 'models/Cat_Recognizer'
-
 
 class CCMobileNetStage:
     def __init__(self):
         # ****Initialize TensorFlow model****
-        # * Load the label map.
-        # Label maps map indices to category names, so that when the convolution
-        # network predicts `5`, we know that this corresponds to `airplane`.
-        # Here we use internal utility functions, but anything that returns a
-        # dictionary mapping integers to appropriate string labels would be fine
 
         # "TF2" way to dynamically load the ssd model from Kaggle Hub and easily "call" the model
         model_files = hub.model_download("tensorflow/ssd-mobilenet-v2/tensorFlow2/ssd-mobilenet-v2")
         self.detect_function = tf.saved_model.load(model_files)
-
-        # Path to frozen detection graph .pb file, which contains the model that is used
-        # for object detection.
-        frozen_model_file: Path = Path(f'{_tensorflow_models_path}/object_detection/'
-                                       f'{_TF_OD_model_name}/{_TF_OD_frozen_model_filename}').resolve()
-
-        # Path to label map file
-        labels_file: Path = Path(f'{_tensorflow_models_path}/object_detection/{_TF_OD_labels_filename}').resolve()
-        label_map = label_map_util.load_labelmap(str(labels_file))
-
-        # Number of classes the object detector can identify
-        num_classes = 90
-        categories = label_map_util.convert_label_map_to_categories(label_map,
-                                                                    max_num_classes=num_classes,
-                                                                    use_display_name=True
-                                                                    )
-        self.category_index = label_map_util.create_category_index(categories)
 
         # Config tensorflow
         config = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=2,
                                           inter_op_parallelism_threads=general_config.max_frame_processor_threads * 2,
                                           allow_soft_placement=True,
                                           device_count={'CPU': 2})
-
-        # * Load the Tensorflow model into memory.
-        detection_graph = tf.Graph()
-        with detection_graph.as_default():
-            od_graph_def = tf.compat.v1.GraphDef()
-            with tf.compat.v2.io.gfile.GFile(str(frozen_model_file), 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
-
-            self.sess = tf.compat.v1.Session(graph=detection_graph, config=config)
-
-        # * Define input and output tensors (i.e. data) for the object detection classifier
-
-        # Input tensor is the image
-        self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-        # Output tensors are the detection boxes, scores, and classes
-        # Each box represents a part of the image where a particular object was detected
-        self.detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-        # Each score represents level of confidence for each of the objects.
-        # The score is shown on the result image, together with the class label.
-        self.detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-
-        # Number of objects detected
-        self.num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
         logger.info(f"TF Config: inter_threads = {tf.config.threading.get_inter_op_parallelism_threads()}")
         logger.info(f"TF Config: intra_threads = {tf.config.threading.get_intra_op_parallelism_threads()}")
@@ -118,20 +50,11 @@ class CCMobileNetStage:
 
         # Perform the actual detection by running the model with the image as input
         start_time = time.time()
-        # (boxes, scores, classes, num) = self.sess.run(
-        #     [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
-        #     feed_dict={self.image_tensor: frame_expanded})
-
-        # detection_result = self.detect_function(tf.convert_to_tensor(frame_expanded, dtype=tf.uint8))
         detection_result = self.detect_function(frame_expanded)
         num = detection_result['num_detections'].numpy()
         scores = detection_result['detection_scores'].numpy()
         classes = detection_result['detection_classes'].numpy()
         boxes = detection_result['detection_boxes'].numpy()
-        # print(f"Num = {num}")
-        # print(f"Scores = {scores}")
-        # print(f"Classes = {classes}")
-        # print(f"Boxes = {boxes}")
 
         end_time = time.time()
         inference_time = end_time - start_time
